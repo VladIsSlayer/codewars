a8747ed0227dc8a4b38a78bf472bd9cb01763945
VladIsSlayer <vladislav3950@mail.ru>
2025-07-12 18:05:38 +0300

    Нахождение количества чисел с нечет кол-вом нечет делителей

---
+++ Odd_Is_Real/Odd_is_real.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+typedef unsigned long long int ULL;
+
+
+
+// Оптимизированная функция подсчёта чисел
+ULL odd_isReal(ULL L, ULL R) {
+    ULL marker,last_divisor;
+    unsigned number;
+    int count = 0, step;
+
+    for(ULL i = L; i<=R; ++i)
+    {
+        marker = 1;
+        number = 0;
+        step = 0;
+        last_divisor = 0;
+
+        while(marker<=i/2+1)
+        {
+            if(i%marker==0)
+            {
+                if (last_divisor==marker)
+                {
+                    break;
+                }
+
+                if(marker%2==1)
+                {
+                    ++number;
+                }
+
+                last_divisor = i/marker;
+
+                if (last_divisor==marker)
+                {
+                    break;
+                }
+                
+                if(last_divisor%2==1)
+                {
+                    ++number;
+                }
+
+                
+                
+                ++marker;
+                
+                marker += step;
+
+                
+            }
+            else{
+                ++marker;
+                ++step;
+            }
+        }
+
+
+
+        if(number%2==1)
+        {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    ULL L = 5;
+    ULL R = 10;
+    std::cout << odd_isReal(L, R) << '\n'; // Output: 2
+    return 0;
+}
